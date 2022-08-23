eb112fe1d575974536d0cc924cb77030c6da85e8
Fnoryn <fabien.ribeaucourt75@gmail.com>
2022-08-23 02:55:41 +0200

    finalisation de la page panier

--- js/cart.js
+++ js/cart.js
@@ -1,84 +1,101 @@
+//inisialisation du local storage
 let localStorageProduit = JSON.parse(localStorage.getItem("produit"));
 console.table(localStorageProduit);
 
 
-
+//fonction pour verifier si le panier est vide et le signaler
 function verifPanierVide() {
 
-    if (localStorageProduit === null || localStorage == 0) {
-
-        window.confirm("votre panier est vide aller d'abort le remplir, cliquez sur OK")
-        window.location.href = "index.html";
+    if (localStorageProduit === null || localStorage == 0 || localStorageProduit == 0) { //si le local storage es vide alors on affiche la fenêtre suivante
+        const balisePvide = document.querySelector("h1")
+        balisePvide.textContent = "votre panier est vide";
         return true;
     }
 }
 
+//fonction pour afficher les produit du panier
 function affichageDuPanier() {
     verifPanierVide();
-
+    
     for (let produit in localStorageProduit) {
         let baliseGeneral = document.querySelector("#cart__items");
+        //création de l'article
+        const idProd = localStorageProduit[produit].idDuProduit;
+        console.log(idProd);
 
+        const couleurProd = localStorageProduit[produit].couleurDuProduit;
+        console.log(couleurProd)
+        
+        const quantiterProd = localStorageProduit[produit].quantiterDuProduit;
+        console.log(quantiterProd);
         const baliseArticle = document.createElement("article")
         baliseGeneral.appendChild(baliseArticle);
         baliseArticle.className = "cart__item";
-        baliseArticle.setAttribute('data-id', localStorageProduit[produit].idDuProduit);
-        baliseArticle.setAttribute('data-color', localStorageProduit[produit].couleurDuProduit);
-        baliseArticle.setAttribute('data-quantiter', localStorageProduit[produit].quantiterDuProduit);
-        const idProd = baliseArticle.getAttribute('data-id');
-        console.log(idProd);
+        baliseArticle.setAttribute('data-id', idProd);
+        baliseArticle.setAttribute('data-color', couleurProd);
+        // baliseArticle.setAttribute('data-quantiter', localStorageProduit[produit].quantiterDuProduit);
+        // récupération de l'id de la couleur et de la quantiter du produit
+   
 
-        const couleurProd = baliseArticle.getAttribute('data-color');
-        console.log(couleurProd);
 
-        const quantiterProd = baliseArticle.getAttribute('data-quantiter');
-        console.log(quantiterProd);
 
+
+        // fetch de l'api pour afficher les img etc.
         fetch(`http://localhost:3000/api/products/` + idProd)
             .then((response) => response.json())
             .then((produit) => {
+                //creation de la div img
                 const baliseDivImg = document.createElement("div")
                 baliseArticle.appendChild(baliseDivImg);
                 baliseDivImg.className = "cart__item__img";
         
+                //creation de l'img
                 const baliseImg = document.createElement("img");
                 baliseDivImg.appendChild(baliseImg);
                 baliseImg.setAttribute('src', `${produit.imageUrl}`);
                 baliseImg.setAttribute('alt', `${produit.altTxt}`);
 
+                //creation de la div du contenu
                 const baliseDivContenu = document.createElement("div");
                 baliseArticle.appendChild(baliseDivContenu);
                 baliseDivContenu.className = "cart__item__content";
-        
+                
+                //creation de la div description dans la div contenu
                 const baliseDivDescription = document.createElement("div");
                 baliseDivContenu.appendChild(baliseDivDescription);
                 baliseDivDescription.className = "cart__item__content__description";
         
+                //creation du titre avec le nom du produit
                 const baliseH2Nom = document.createElement("h2");
                 baliseDivDescription.appendChild(baliseH2Nom)
                 baliseH2Nom.textContent = `${produit.name}`;
         
+                //creation du p pour afficher la couleur du produit
                 const balisePCouleur = document.createElement("p");
                 baliseDivDescription.appendChild(balisePCouleur)
                 balisePCouleur.textContent = `${couleurProd}`;
         
+                //creation du p pour afficher le prix du produit
                 const balisePPrix = document.createElement("p");
                 baliseDivDescription.appendChild(balisePPrix);
-                balisePPrix.textContent = `${produit.price}`;
+                balisePPrix.textContent = `${produit.price} €`;
         
-        
+                //creation de la div parametre pour la quantiter etc.
                 const baliseDivContenuParametre = document.createElement("div");
                 baliseArticle.appendChild(baliseDivContenuParametre);
                 baliseDivContenuParametre.className = "cart__item__content__settings";
-        
+                
+                //creation de la div quantiter
                 const baliseDivQuantiter = document.createElement("div");
                 baliseDivContenuParametre.appendChild(baliseDivQuantiter);
                 baliseDivQuantiter.className = "cart__item__content__settings__quantity";
 
+                //creation du p pour afficher la quantiter
                 const balisePQuantiter = document.createElement("p");
                 baliseDivQuantiter.appendChild(balisePQuantiter)
                 balisePQuantiter.textContent = "Qté : ";
 
+                //creation du input pour afficher et géré de façon dynamique la quantiter des produits 
                 const baliseInput = document.createElement("input");
                 baliseDivQuantiter.appendChild(baliseInput);
                 baliseInput.type = 'number';
@@ -87,27 +104,104 @@ function affichageDuPanier() {
                 baliseInput.min = '1';
                 baliseInput.max = '100';
                 baliseInput.value = `${quantiterProd}`;
+                baliseInput.addEventListener("change", (event) => {
+                    changeQuantiter(event.target);
+                })
+
                 
+                //creation de la div pour supprimer les produits 
                 const baliseDivSupprimer = document.createElement("div");
                 baliseArticle.appendChild(baliseDivSupprimer);
                 baliseDivSupprimer.className = "cart__item__content__settings__delete";
+                baliseDivSupprimer.addEventListener("click", (event) => {
+                    suppItem(event.target);
+                })
                 
+                //creation du p pour supprimer les produit
                 const balisePSupprimer = document.createElement("p");
                 baliseDivSupprimer.appendChild(balisePSupprimer);
                 balisePSupprimer.className = "deleteItem";
                 balisePSupprimer.textContent = "Supprimer";
 
+                prixTotal();
+                quantiterTotal()
             })
 
     }
 }
 affichageDuPanier();
 
+function suppItem(){
+    let btnSupp = document.querySelectorAll('.deleteItem');
+
+    for(let i =0; i < btnSupp.length; i++){
+        btnSupp[i].addEventListener("click", (event) => {
+            event.preventDefault();
+            let idSelectioner = btnSupp[i].closest("article").dataset.id;
+            console.log(idSelectioner);
+            let couleurSelectioner = btnSupp[i].closest("article").dataset.color;
+            console.log(couleurSelectioner);
+            localStorageProduit = localStorageProduit.filter(el => el.idDuProduit !== idSelectioner || el.couleurDuProduit !== couleurSelectioner);
+            console.log(localStorageProduit);
+            localStorage.setItem("produit", JSON.stringify(localStorageProduit));
+            location.reload();
+
+        })
+    }
+}
+
+function changeQuantiter(){
+    let produitQuantiter = document.querySelectorAll(".itemQuantity");
+
+    produitQuantiter.forEach((produitQt) => {
+        let idProduitQt = produitQt.closest("article").dataset.id;
+        console.log(idProduitQt);
+        let couleurProduitQT = produitQt.closest("article").dataset.color;
+        console.log(couleurProduitQT);
+        produitQt.addEventListener("click", (event) => {
+            event.preventDefault();
+            let nouvelQuantiter = Number(produitQt.value);
+            localStorageProduit.forEach((element) => {
+                if(element.idDuProduit == idProduitQt && element.couleurDuProduit == couleurProduitQT){
+                    element.quantiterDuProduit = nouvelQuantiter;
+                    location.reload();
+                }
+            });
+            localStorage.setItem("produit", JSON.stringify(localStorageProduit));
+  
+        })
+    })
 
 
+}
 
 
-let nameRegex = /^[a-zA-Z\-çñàéèêëïîôüù ]{2,}$/;
+function prixTotal(){
+    let quantiter = document.querySelectorAll(".itemQuantity");
+    let prix = document.querySelectorAll(".cart__item__content__description");
+    let prixPanier = 0;
+
+    for(let i=0; i < prix.length; i++){
+        prixPanier += parseInt(prix[i].lastElementChild.textContent) * quantiter[i].value;
+    }
+    document.querySelector('#totalPrice').textContent = prixPanier;
+    localStorage.setItem("produit", JSON.stringify(localStorageProduit));
+
+}
+
+function quantiterTotal(){
+    let produitTotal = JSON.parse(localStorage.getItem("produit"));
+    let totalProduit = 0;
+    for(const item of produitTotal){
+        totalProduit += parseInt(item.quantiterDuProduit);
+    }
+    const quantiterTotalProduit = document.querySelector('#totalQuantity');
+    quantiterTotalProduit.textContent = totalProduit;
+    localStorage.setItem("produit", JSON.stringify(localStorageProduit));
+}
+
+function formulaire(){
+    let nameRegex = /^[a-zA-Z\-çñàéèêëïîôüù ]{2,}$/;
 let adressRegex = /^[0-9a-zA-Z\s,.'-çñàéèêëïîôüù]{3,}$/;
 let emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 // Variables pour récupérer les id des champs de formulaire
@@ -171,3 +265,62 @@ email.addEventListener("change", (event) => {
         return true;
     }
 });
+
+}
+
+formulaire();
+
+function envoiInfoLocalStorage(){
+    const brnCommander = document.querySelector('#order');
+
+    brnCommander.addEventListener("click", (event)=> {
+        event.preventDefault();
+
+        let inputPrenom = document.querySelector('#firstName');
+        let inputNom = document.querySelector('#lastName');
+        let inputAdresse = document.querySelector('#address');
+        let inputVille = document.querySelector('#city');
+        let inputEmail = document.querySelector('#email');
+
+        let produitsId = [];
+        for(let i=0; i<localStorageProduit.length; i++){
+            produitsId.push(localStorageProduit[i].idDuProduit);
+        }
+        console.log(produitsId);
+
+        const commande = {
+            contact : {
+                fistName: inputPrenom.value,
+                lastName: inputNom.value,
+                address: inputAdresse.value,
+                city: inputVille.value,
+                email: inputEmail.value,
+            },
+            produit: produitsId,
+        }
+
+        const option = {
+            method: 'POST',
+            body: JSON.stringify(commande),
+            headers: {
+                'Accept': 'application/json',
+                "Content-Type": "application/json"
+            },
+        };
+
+        fetch("http://localhost:3000/api/products/order", option)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            localStorage.clear();
+            localStorage.setItem("commandeId", data.orderId);
+
+            document.location.href = "confirmation.html";
+        })
+        .catch((err) => {
+            alert("pb avec le fetch :" + err.message);
+        });
+    })
+}
+
+envoiInfoLocalStorage();
